51c58a8e09cf63cb9dbb6e7bf636cc6db62c29a2
ayushkumar5221@gmail.com <ayushkumar.2024ug1009@iiitranchi.ac.in>
2026-03-16 20:00:09 +0530

    setting up env

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-require('dotenv').config()
+const {PORT} = require('./config/serverConfig');
 
 const setupAndStartServer  =  async ()=>{
-    // create the express object
+    // create the express object 
     const app = express();
-    const PORT = 3000;
+    // const PORT = 3000;
     app.listen(PORT, ()=>{
         console.log(`Server started at ${PORT}`)
-        console.log(process.env);
+        // console.log(process.env);
         
     })
 
